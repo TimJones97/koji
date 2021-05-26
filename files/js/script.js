7fc7abb91879656c228084e5381c64b78146aff2
Tim Jones <t27.jones@connect.qut.edu.au>
2021-05-26 21:37:56 +1000

    Added contact section, added resize functionality to highdef background

--- files/js/script.js
+++ files/js/script.js
@@ -1,5 +1,8 @@
-var rotation = 0,
-	scale = 1.0;
+var navRotation = 0,
+	navScale = 1.0,
+	highdefBackgroundScale = 1.0;
+	highdefTop = 0;
+	highdefRight = 0;
 
 function changeNav(){
 	$('section').each(function(){
@@ -35,23 +38,23 @@ function rotateCircleNav(index, nextIndex, direction){
 	var currentIndex = nextIndex - index;
 	if(direction == 'down' && currentIndex == 1){
 		// Increment the rotation by 30 degrees each time
-		rotation += 30;
+		navRotation += 30;
 		$('.circle-nav .active').removeClass('active').next().addClass('active');
 	}
 	else if(direction == 'down' && currentIndex == 2){
-		rotation += 60;
+		navRotation += 60;
 		$('.circle-nav .active').removeClass('active').next().addClass('active');
 	}
 	else if(direction == 'up' && currentIndex == -2){
-		rotation = rotation - 60;
+		navRotation = navRotation - 60;
 		$('.circle-nav .active').removeClass('active').next().addClass('active');
 	}
 	else {
 		// Reverse rotation by 30 degrees to animate backward
-		rotation = rotation - 30;
+		navRotation = navRotation - 30;
 		$('.circle-nav .active').removeClass('active').prev().addClass('active');
 	}
-	$('.circle-nav').css('transform', 'scale3d(' + scale + ',' + scale + ', 1.0) rotate(' + rotation + 'deg)');
+	$('.circle-nav').css('transform', 'scale3d(' + navScale + ',' + navScale + ', 1.0) rotate(' + navRotation + 'deg)');
 }
 // Fade in the video after the window is fully loaded
 function fadeVideoLoad(){
@@ -64,7 +67,6 @@ function scaleCircleNav(){
 	var maxVHeight = 1080,
 		height = $(window).height(),
 		width = $(window).width();
-		console.log(width / height);
 
 	if (height > maxVHeight || (width / height) >  1.5){
 		scale = height / maxVHeight;
@@ -77,14 +79,35 @@ function scaleCircleNav(){
 	}
 
 	// Add the new scale and rotation
-	$('.circle-nav').css('transform', 'scale3d(' + scale + ',' + scale + ', 1.0) rotate(' + rotation + 'deg)');
+	$('.circle-nav').css('transform', 'scale3d(' + navScale + ',' + navScale + ', 1.0) rotate(' + navRotation + 'deg)');
+}
+function scaleHighdefBackground(){
+	var minVWidth = 1920,
+		width = $(window).width();
+	if(width < minVWidth){
+		highdefBackgroundScale = width / minVWidth;
+		highdefTop = (1.0 - (width / minVWidth)) / 2 * 100;
+		highdefRight = (1.0 - (width / minVWidth)) / 2 * 100;
+		
+	}
+	else {
+		highdefBackgroundScale = 1.0;
+		highdefTop = 0;
+		highdefRight = 0;
+	}
+	// Add the styles to the dynamic background in the high definition section
+	$('.highdef-bg').css('transform', 'scale3d(' + highdefBackgroundScale + ',' + highdefBackgroundScale + ', 1.0)');
+	$('.highdef-bg').css('top',  '-' + highdefTop + '%');
+	$('.highdef-bg').css('right','-' + highdefRight + '%');
 }
 $(window).resize(function(){
 	scaleCircleNav();
+	scaleHighdefBackground();
 });
 $(document).ready(function() {
 	fadeVideoLoad();
 	scaleCircleNav();
+	scaleHighdefBackground();
   	$('#fullpage-anim').pagepiling({
 	  	menu: '.circles',
 		anchors: ['page1', 'page2', 'page3'],
